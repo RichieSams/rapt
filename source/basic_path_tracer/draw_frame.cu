6dff9991b7b8c284ac3b80f0dde122d80a1ec6eb
Adrian Astley <adastley@gmail.com>
2015-03-03 15:35:59 -0600

    BASIC_PATH_TRACER: #include device_launch_parameters.h in order to have intellisense for thread/block variables
    The CUDA compiler includes these already, but we manually include them in order to give visual studio a hand.

--- source/basic_path_tracer/draw_frame.cu
+++ source/basic_path_tracer/draw_frame.cu
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
 
 __global__ void cuda_kernel_texture_2d(unsigned char *surface, int width, int height, size_t pitch, float t) {
