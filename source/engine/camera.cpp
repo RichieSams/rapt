2f7b8c67497d56b2c1ebeea4818443b57282e92c
Adrian Astley <adastley@gmail.com>
2015-03-04 19:36:49 -0600

    ENGINE: Add a camera class
    Using the class from The Halfling Project as a basis

---
+++ source/engine/camera.cpp
@@ -0,0 +1,83 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013 - 2015
+ */
+
+/**
+ * Modified for use in raic - RichieSam's Adventures in Cuda
+ * raic is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2015
+ */
+
+#include "engine/camera.h"
+
+#include <algorithm>
+
+#define PI 3.141592654f
+#define TWO_PI 6.283185307f
+
+
+namespace Engine {
+
+void HostCamera::Rotate(float dTheta, float dPhi) {
+	if (m_up > 0.0f) {
+		m_theta += dTheta;
+	} else {
+		m_theta -= dTheta;
+	}
+
+	m_phi += dPhi;
+
+	// Keep phi within -2PI to +2PI for easy 'up' comparison
+	if (m_phi > TWO_PI) {
+		m_phi -= TWO_PI;
+	} else if (m_phi < -TWO_PI) {
+		m_phi += TWO_PI;
+	}
+
+	// If phi is between 0 to PI or -PI to -2PI, make 'up' be positive Y, other wise make it negative Y
+	if ((m_phi > 0 && m_phi < PI) || (m_phi < -PI && m_phi > -TWO_PI)) {
+		m_up = 1.0f;
+	} else {
+		m_up = -1.0f;
+	}
+}
+
+void HostCamera::Zoom(float distance) {
+	m_radius -= distance;
+
+	// Don't let the radius go negative
+	// If it does, re-project our target down the look vector
+	if (m_radius <= 0.0f) {
+		m_radius = 30.0f;
+		float3 look = normalize(m_target - GetCameraPosition());
+		m_target += look * 30.0f;
+	}
+}
+
+void HostCamera::Pan(float dx, float dy) {
+	float3 look = normalize(m_target - GetCameraPosition());
+	float3 worldUp = make_float3(0.0f, m_up, 0.0f);
+
+	float3 left = normalize(cross(look, worldUp));
+	float3 up = normalize(cross(look, left));
+
+	m_target += -(left * dx) + (up * dy);
+}
+
+DeviceCamera HostCamera::GetDeviceCamera() const {
+	float3 worldUp = make_float3(0.0f, m_up, 0.0f);
+
+	DeviceCamera camera;
+	camera.origin = GetCameraPosition();
+	camera.z = normalize(m_target - camera.origin);
+	camera.tanFovDiv2_X = m_tanFovDiv2_X;
+	camera.x = normalize(cross(camera.z, worldUp));
+	camera.tanFovDiv2_Y = m_tanFovDiv2_Y;
+	camera.y = normalize(cross(camera.z, camera.x));
+
+	return camera;
+}
+
+} // End of namespace Scene
---
+++ source/engine/camera.h
@@ -0,0 +1,114 @@
+/* The Halfling Project - A Graphics Engine and Projects
+ *
+ * The Halfling Project is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2013 - 2015
+ */
+
+/**
+ * Modified for use in raic - RichieSam's Adventures in Cuda
+ * raic is the legal property of Adrian Astley
+ * Copyright Adrian Astley 2015
+ */
+
+#pragma once
+
+#include <graphics/helper_math.h>
+
+
+struct DeviceCamera {
+	float3 x;
+	float tanFovDiv2_X;
+	float3 y;
+	float tanFovDiv2_Y;
+	float3 z;
+	float3 origin;
+};
+
+namespace Engine {
+
+/** 
+ * A class for emulating a camera looking into the scene. 
+ * It stores the view matrix and projection matrix for use by the renderer
+ * and provides methods for moving the camera around the scene
+ */
+class HostCamera {
+public:
+	HostCamera() 
+		: m_theta(0.0f), 
+		  m_phi(0.0f), 
+		  m_radius(0.0f), 
+		  m_up(1.0f),
+		  m_tanFovDiv2_X(tan(0.785398f /* PI / 4 */)),
+		  m_tanFovDiv2_Y(tan(0.785398f * 19.0f / 10.0f)),
+		  m_target(make_float3(0.0f, 0.0f, 0.0f)) {
+	}
+	HostCamera(float theta, float phi, float radius) 
+		: m_theta(theta), 
+		  m_phi(phi), 
+		  m_radius(radius), 
+		  m_up(1.0f),
+		  m_tanFovDiv2_X(tan(0.785398f /* PI / 4 */)),
+		  m_tanFovDiv2_Y(tan(0.785398f * 19.0f / 10.0f)),
+		  m_target(make_float3(0.0f, 0.0f, 0.0f)) {
+	}
+
+private:
+	float m_theta;
+	float m_phi;
+	float m_radius;
+	float m_up;
+
+	float m_tanFovDiv2_X;
+	float m_tanFovDiv2_Y;
+
+	float3 m_target;
+
+public:
+	/**
+	 * Rotate the camera about a point in front of it (m_target). Theta is a rotation 
+	 * that tilts the camera forward and backward. Phi tilts the camera side to side. 
+	 *
+	 * @param dTheta    The number of radians to rotate in the theta direction
+	 * @param dPhi      The number of radians to rotate in the phi direction
+	 */
+	void Rotate(float dTheta, float dPhi);
+	/**
+	 * Move the camera down the look vector, closer to m_target. If we overtake m_target,
+	 * it is reprojected 30 units down the look vector
+	 *
+	 * TODO: Find a way to *not* hard-code the reprojection distance. Perhaps base it on the 
+	 *       scene size? Or maybe have it defined in an settings.ini file
+	 *
+	 * @param distance    The distance to zoom. Negative distance will move the camera away from the target, positive will move towards
+	 */
+	void Zoom(float distance);
+	/**
+	 * Moves the camera within its local X-Y plane
+	 *
+	 * @param dx    The amount to move the camera right or left
+	 * @param dy    The amount to move the camera up or down
+	 */
+	void Pan(float dx, float dy);
+
+	inline void SetProjection(float fov, float aspectRatio) {
+		m_tanFovDiv2_X = tan(fov * 0.5f);
+		m_tanFovDiv2_Y = tan(fov * 0.5f / aspectRatio);
+	}
+
+	/**
+	 * Returns the position of the camera in Cartesian coordinates
+	 *
+	 * @return    The position of the camera
+	 */
+	inline float3 GetCameraPosition() const { 
+		float x = m_radius * sinf(m_phi) * sinf(m_theta);
+		float y = m_radius * cosf(m_phi);
+		float z = m_radius * sinf(m_phi) * cosf(m_theta);
+
+		return m_target + make_float3(x, y, z);
+	}
+
+	DeviceCamera GetDeviceCamera() const;
+};
+
+} // End of namespace Scene
