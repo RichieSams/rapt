a04d20a7acefeb2c434ea135486a05aad439be5a
Adrian Astley <adastley@gmail.com>
2015-03-12 10:25:53 -0500

    BASIC_PATH_TRACER: Create a method for generating a random direction in the hemisphere from a normal

--- source/basic_path_tracer/path_trace.cu
+++ source/basic_path_tracer/path_trace.cu
@@ -112,6 +112,27 @@ __device__ float TestRayPlaneIntersection(Scene::Ray &ray, Scene::Plane &plane, 
 	return dot(plane.Normal, plane.Point - ray.Origin) / denominator;
 }
 
+__device__ float3 CreateRandomDirectionInNormalHemisphere(float3 normal, curandState *randState) {
+	// Create a random coordinate in spherical space
+	float randAngle = TWO_PI * curand_uniform(randState);
+	float randDistance = curand_uniform(randState);
+	float distanceFromCenter = sqrt(randDistance);
+	
+	// Find an axis that is not parallel to normal.x
+	float3 majorAxis = abs(normal.x > 0.1f) ? make_float3(0.0f, 1.0f, 0.0f) : make_float3(1.0f, 0.0f, 0.0f);
+
+	// Use majorAxis to create a coordinate system relative to world space
+	float3 u = normalize(cross(majorAxis, normal));
+	float3 v = cross(normal, u);
+	float3 w = normal;
+
+	// Transform from spherical coordinates to the cartesian coordinates space
+	// we just defined above, then use the definition to transform to world space
+	return normalize(u * cos(randAngle) * distanceFromCenter +
+	                 v * sin(randAngle) * distanceFromCenter +
+	                 w * sqrt(1.0f - randDistance));
+}
+
 __global__ void PathTraceKernel(unsigned char *textureData, uint width, uint height, size_t pitch, DeviceCamera *g_camera, Scene::Sphere *g_spheres, uint numSpheres, uint hashedFrameNumber) {
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
