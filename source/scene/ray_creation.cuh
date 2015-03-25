b26aa17dd146dade09d1a2dc8e3b55c8b7a96781
Adrian Astley <adastley@gmail.com>
2015-03-24 23:15:49 -0500

    SCENE: Create a function for generating a cosine weighted random direction

--- source/scene/ray_creation.cuh
+++ source/scene/ray_creation.cuh
@@ -73,3 +73,45 @@ __device__ float3 CreateUniformDirectionInHemisphere(float3 normal, curandState 
 }
 
 
+/**
+ * Creates a random direction in the hemisphere defined by the normal, weighted by a cosine lobe  
+ *
+ * Based off the algorithm and code in 'Physically Based Rendering, 2nd Ed.' - Pharr & Humphreys
+ * Chapter 13 - Example 13.6.2 & 13.63 - Pgs. 664 to 669
+ *
+ * @param normal        The normal that defines the hemisphere
+ * @param randState     The random state to use for internal random number generation        
+ * @return              A cosine weighted random direction in the hemisphere
+ */
+__device__ float3 CreateCosineWeightedDirectionInHemisphere(float3 normal, curandState *randState) {
+	// Create a random coordinate in spherical space
+	float theta = TWO_PI * curand_uniform(randState);
+	float r = curand_uniform(randState);
+	
+	// The position in local space
+	float x = r * cos(theta);
+	float y = r * sin(theta);
+
+	// Find an axis that is not parallel to normal
+	float3 majorAxis;
+	if (abs(normal.x) < INV_SQRT_THREE) { 
+		majorAxis = make_float3(1, 0, 0);
+	} else if (abs(normal.y) < INV_SQRT_THREE) { 
+		majorAxis = make_float3(0, 1, 0);
+	} else {
+		majorAxis = make_float3(0, 0, 1);
+	}
+
+	// Use majorAxis to create a coordinate system relative to world space
+	float3 u = normalize(cross(majorAxis, normal));
+	float3 v = cross(normal, u);
+	float3 w = normal;
+
+	// Transform from local coordinates to world coordinates
+	// Z is projected from the unit disk up to the hemisphere
+	//     This creates the cosine weighting. 
+	//     This is known as "Mallay's Method"
+	return normalize(u * x +
+	                 v * y +
+	                 w * sqrt(1.0f - x * x - y * y));
+}
