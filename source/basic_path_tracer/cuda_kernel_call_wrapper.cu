a167428f5dfb07477cb88bdecaf5353a41907953
Adrian Astley <adastley@gmail.com>
2015-03-24 23:16:51 -0500

    BASIC_PATH_TRACER: Return any errors thrown by the kernel call to the main app
    And let it handle them. Rather than trying to include anything in the .cu file

--- source/basic_path_tracer/cuda_kernel_call_wrapper.cu
+++ source/basic_path_tracer/cuda_kernel_call_wrapper.cu
@@ -22,17 +22,12 @@ uint32 WangHash(uint32 a) {
  * in a function. We forward declare the function in other parts of the project and
  * implement the functions here, in this .cu file.
  */
-void PathTraceNextFrame(void *buffer, uint width, uint height, size_t pitch, DeviceCamera *camera, Scene::SceneObjects *sceneObjects, uint frameNumber) {
-	cudaError_t error = cudaSuccess;
-
+cudaError PathTraceNextFrame(void *buffer, uint width, uint height, size_t pitch, DeviceCamera *camera, Scene::SceneObjects *sceneObjects,  uint frameNumber) {
 	dim3 Db = dim3(16, 16);   // block dimensions are fixed to be 256 threads
 	dim3 Dg = dim3((width + Db.x - 1) / Db.x, (height + Db.y - 1) / Db.y);
 
 	// The actual kernel launch call
 	PathTraceKernel<<<Dg, Db>>>((unsigned char *)buffer, width, height, pitch, camera, sceneObjects, WangHash(frameNumber));
 
-	error = cudaGetLastError();
-	if (error != cudaSuccess) {
-		//exit(error);
-	}
+	return cudaGetLastError();
 }
